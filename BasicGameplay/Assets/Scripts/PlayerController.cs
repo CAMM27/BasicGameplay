7bde1b41572ce4b0177bf0e1c873b66f0e4a9553
CAMM27 <oneillc@roverkids.org>
2024-11-26 13:51:31 -0500

    spacebar launches steak

--- BasicGameplay/Assets/Scripts/PlayerController.cs
+++ BasicGameplay/Assets/Scripts/PlayerController.cs
@@ -28,6 +28,13 @@ public class playercontrol : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);            
+        }
+         
         
     }
 }
