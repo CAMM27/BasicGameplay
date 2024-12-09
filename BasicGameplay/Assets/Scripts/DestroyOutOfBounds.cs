88b308573b3de53e4d7f4cb5c9f81178fa44c691
CAMM27 <oneillc@roverkids.org>
2024-12-09 14:12:33 -0500

    debug says game over end 2.4

--- BasicGameplay/Assets/Scripts/DestroyOutOfBounds.cs
+++ BasicGameplay/Assets/Scripts/DestroyOutOfBounds.cs
@@ -7,21 +7,23 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // if object goes past players view remove that obkject.
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
         }
-        else if (transform.position.z < lowerBound) 
+        else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
 
-        
+
     }
 }
--- BasicGameplay/Assets/Scripts/SpawnManager.cs
+++ BasicGameplay/Assets/Scripts/SpawnManager.cs
@@ -6,8 +6,8 @@ public class SpawnManager : MonoBehaviour
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
     private float startDelay = 2;
-    private float spawnInterval = 1f;
-    
+    private float spawnInterval = 1.5f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,12 +17,12 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
 
-        
+
+
     }
 
-    void SpawnRandomAnimal ()
+    void SpawnRandomAnimal()
     {
         int animalIndex = Random.Range(0, animalPrefabs.Length);
         Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
